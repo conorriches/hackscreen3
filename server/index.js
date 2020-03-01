21b077a62268a8bc22e2b52d4c6e3e858352070c
Conor R <conor.riches@gmail.com>
2020-03-01 21:05:54 +0000

    Update to include local serving

--- server/index.js
+++ server/index.js
@@ -3,11 +3,20 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const express = Express();
 const App = require("./app.js");
+const cors = require("cors");
 
 const app = new App();
+express.use(cors());
+
+express.use(Express.static("build"));
 
 express.get("/ping", function(req, res) {
   return res.send("pong");
 });
 
+express.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "../", "build", "index.html"));
+});
+
+console.log(process.env.PORT);
 express.listen(process.env.PORT || 5000);
